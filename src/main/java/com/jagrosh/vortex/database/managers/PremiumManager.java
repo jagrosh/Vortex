1e3cb599ddc65039040c63b4cc21ac3e4ad4306c
Dan // pixelFlow <dan@pixelflow.xyz>
2020-06-02 23:37:44 +0100

    Removed 'does does'
    :)

--- src/main/java/com/jagrosh/vortex/database/managers/PremiumManager.java
+++ src/main/java/com/jagrosh/vortex/database/managers/PremiumManager.java
@@ -187,7 +187,7 @@ public class PremiumManager extends DataManager
         public String getFooterString()
         {
             if(level==Level.NONE)
-                return "This server does does not have Vortex Plus or Vortex Pro";
+                return "This server does not have Vortex Plus or Vortex Pro";
             if(until.getEpochSecond()==Instant.MAX.getEpochSecond())
                 return "This server has " + level.name + " permanently";
             return "This server has " + level.name + " until";
