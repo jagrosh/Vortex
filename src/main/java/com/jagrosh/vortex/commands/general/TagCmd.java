ed1e61f1c48240a9edb7026adc4e73f68b35ee0f
TheCaptainYaya <65315989+TheCaptainYaya@users.noreply.github.com>
2020-06-10 13:07:11 -0700

    Update TagCmd.java

--- src/main/java/com/jagrosh/vortex/commands/general/TagCmd.java
+++ src/main/java/com/jagrosh/vortex/commands/general/TagCmd.java
@@ -3,8 +3,8 @@ package com.jagrosh.vortex.commands.general;
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import com.jagrosh.vortex.Vortex;
+import com.jagrosh.vortex.commands.CommandTools;
 import net.dv8tion.jda.core.Permission;
-import net.dv8tion.jda.core.entities.Role;
 
 public class TagCmd extends Command
 {
@@ -20,19 +20,7 @@ public class TagCmd extends Command
     @Override
     protected void execute(CommandEvent event)
     {
-        Role rtcRole = vortex.getDatabase().settings.getSettings(event.getGuild()).getRtcRole(event.getGuild());
-        Role modRole = vortex.getDatabase().settings.getSettings(event.getGuild()).getModeratorRole(event.getGuild());
-
-        /* Checks if the user has permission to use tags. People may use tags if they meet any of the following criteria:
-            - Has the manage messages permission
-            - Has the predesignated moderator role
-            - Is an RTC
-         */
-        if (
-                !event.getMember().hasPermission(event.getTextChannel(), Permission.MESSAGE_MANAGE) &&
-                !(modRole != null && event.getMember().getRoles().contains(modRole)) &&
-                !(rtcRole != null && event.getMember().getRoles().contains(rtcRole))
-        )
+        if (!CommandTools.hasGeneralCommandPerms(vortex, event, Permission.MESSAGE_MANAGE))
             return;
 
         String tagName = event.getArgs().trim().split(" ")[0];
